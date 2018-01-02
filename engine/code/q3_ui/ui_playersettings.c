cbf35dd168067c892907afdc1fff33fcc50b3eea
cbeattie <cbeattie@google.com>
2018-01-02 17:02:17 +0000

    Update copyright notices

--- engine/code/q3_ui/ui_playersettings.c
+++ engine/code/q3_ui/ui_playersettings.c
@@ -1,6 +1,6 @@
 /*
 ===========================================================================
-Copyright (C) 1999-2005 Id Software, Inc.
+Copyright (C) 1999-2005 Id Software, Inc., 2016 Google Inc.
 
 This file is part of Quake III Arena source code.
 
