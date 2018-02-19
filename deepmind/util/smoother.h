c547d196d753ec7218e334cf0407199d61937670
cbeattie <cbeattie@google.com>
2018-02-19 12:25:24 +0000

    [util] Add mathematical smoothing functions.

---
+++ deepmind/util/smoother.h
@@ -0,0 +1,138 @@
+#ifndef DML_DEEPMIND_UTIL_SMOOTHER_H_
+#define DML_DEEPMIND_UTIL_SMOOTHER_H_
+
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+namespace deepmind {
+namespace lab {
+namespace util {
+namespace internal {
+
+// Generic smoothing class. Specific implementation specified by SmoothFunctor.
+template <typename T, typename DiffT, typename SmoothFunctor>
+class Smoother {
+ public:
+  // 'smooth_time' is used by the SmoothFunctor as part of the smoothing
+  // process. Larger means converges faster. 'start' is the starting value for
+  // the smoother.
+  Smoother(double smooth_time, const T& start)
+      : target_(start),
+        current_(start),
+        velocity_(start - start),  // Start with zero velocity.
+        smooth_functor_(smooth_time) {}
+
+  // Set target to smooth towards.
+  void set_target(T target) { target_ = target; }
+
+  // Current position of smoother. Updated during 'Update' to move towards
+  // target().
+  const T& current() const { return current_; }
+
+  void set_velocity(const DiffT& velocity) { velocity_ = velocity; }
+  void set_current(T current) { current_ = current; }
+  const T& target() const { return target_; }
+  const DiffT& velocity() const { return velocity_; }
+
+  // Sets current to target and clears velocity;
+  void SnapToTarget() {
+    current_ = target_;
+    velocity_ = current_ - current_;
+  }
+
+  // Update current_ to target_.
+  void Update(double delta_time) {
+    smooth_functor_.Update(delta_time, target_, &velocity_, &current_);
+  }
+
+ private:
+  T target_;        // Value smoothing towards.
+  T current_;       // Smoothed value
+  DiffT velocity_;  // velocity of current_ towards target_.
+  SmoothFunctor smooth_functor_;
+};
+
+class CriticalDamper {
+ public:
+  // 'smooth_time' is used to calculate the spring strength.
+  // If 'smooth_time' is greater than zero the spring strength will be:
+  // (2.0 * mass / smooth_time)^2.
+  // Otherwise a special case is observed and 'current()' will be set to the
+  // 'target()' and 'velocity()' will be set to zero instantly after calls to
+  // 'Update(delta_time)' no matter the 'delta_time'.
+  explicit CriticalDamper(double smooth_time)
+      : omega_(smooth_time > 0.0 ? 2.0 / smooth_time : 0) {}
+  // Models a critically damped spring with omega_ (equal to sqrt(k/m)).
+  template <typename T, typename DiffT>
+  void Update(double delta_time, const T& target, DiffT* velocity, T* current) {
+    if (omega_ <= 0) {
+      *current = target;
+      *velocity = target - target;  // Zero the velocity.
+    } else if (delta_time > 0.0) {
+      DiffT gap = *current - target;
+      DiffT temp = (*velocity + omega_ * gap) * delta_time;
+      double e = std::exp(-omega_ * delta_time);
+      *velocity = (*velocity - omega_ * temp) * e;
+      *current = target + (gap + temp) * e;
+    }
+  }
+
+ private:
+  double omega_;
+};
+
+// Frame-rate independent model of approaching target by taking the gap and
+// gap *= 1.0 - 1.0 / (smooth_time + 1.0) every frame at 60 fps.
+class ExponentialDecayAt60 {
+ public:
+  explicit ExponentialDecayAt60(double smooth_time) {
+    if (smooth_time > 0.0) {
+      double ratio = 1.0 - 1.0 / (smooth_time + 1.0);
+      decay_rate_ = -60.0 * std::log(ratio);
+      inv_at_60_decay_ = 1.0 / ratio;
+    } else {
+      decay_rate_ = 0;
+      inv_at_60_decay_ = 0;
+    }
+  }
+
+  template <typename T, typename DiffT>
+  void Update(double delta_time, const T& target, DiffT* velocity, T* current) {
+    if (decay_rate_ <= 0) {
+      *current = target;
+      *velocity = target - target;
+    } else if (delta_time > 0.0) {
+      double decay = std::exp(-delta_time * decay_rate_);
+      DiffT gap = *current - target;
+      DiffT new_gap = decay * gap;
+      DiffT prev_gap_at60 = new_gap * inv_at_60_decay_;
+      *velocity = (new_gap - prev_gap_at60) * 60.0;
+      *current = target + new_gap;
+    }
+  }
+
+ private:
+  double decay_rate_;
+  double inv_at_60_decay_;
+};
+
+}  // namespace internal
+
+inline double ConvertExpAt60FpsToSmoothTime(double e) { return (1.0 - e) / e; }
+
+// 'smooth_time' must be greater or equal to 0.
+template <typename T, typename DiffT = T>
+using SmoothCriticalDamped =
+    internal::Smoother<T, DiffT, internal::CriticalDamper>;
+
+// 'smooth_time' must be in range [0, 1].
+template <typename T, typename DiffT = T>
+using SmoothExponentialDecayAt60 =
+    internal::Smoother<T, DiffT, internal::ExponentialDecayAt60>;
+
+}  // namespace util
+}  // namespace lab
+}  // namespace deepmind
+
+#endif  // DML_DEEPMIND_UTIL_SMOOTHER_H_
---
+++ deepmind/util/smoother_test.cc
@@ -0,0 +1,171 @@
+#include "deepmind/util/smoother.h"
+
+#include "gtest/gtest.h"
+#include "Eigen/Dense"
+
+namespace deepmind {
+namespace lab {
+namespace util {
+namespace {
+
+TEST(SmootherTest, TimeStepSizeInvariant) {
+  for (int test = 0; test < 1; ++test) {
+    double target = std::pow(2.0, test);
+    SmoothCriticalDamped<double> damper1(/*smooth_time=*/2.0 / 0.5,
+                                         /*start=*/0.0);
+    damper1.set_target(target);
+    SmoothCriticalDamped<double> damper2 = damper1;
+    damper1.Update(10.0);
+    for (int i = 0; i < 100; ++i) {
+      damper2.Update(0.1);
+    }
+    EXPECT_NEAR(damper1.current(), damper2.current(), 1e-5);
+    EXPECT_NEAR(damper1.velocity(), damper2.velocity(), 1e-5);
+  }
+}
+
+TEST(SmootherTest, TimeStepSizeInvariantExpDecay) {
+  for (int test = 0; test < 1; ++test) {
+    double target = std::pow(2.0, test);
+    SmoothExponentialDecayAt60<double> damper1(
+        /*smooth_time=*/ConvertExpAt60FpsToSmoothTime(0.1), /*start=*/0.0);
+    damper1.set_target(target);
+    SmoothExponentialDecayAt60<double> damper2 = damper1;
+    damper1.Update(10.0);
+    for (int i = 0; i < 100; ++i) {
+      damper2.Update(0.1);
+    }
+    EXPECT_NEAR(damper1.current(), damper2.current(), 1e-5);
+    EXPECT_NEAR(damper1.velocity(), damper2.velocity(), 1e-5);
+  }
+}
+
+TEST(SmootherTest, ApproachesTarget) {
+  for (int test = 0; test < 1; ++test) {
+    double target = std::pow(2.0, test);
+    SmoothCriticalDamped<double> damper(/*smooth_time=*/4.0, /*start=*/0.0);
+    damper.set_target(target);
+    double old_current = damper.current();
+    for (int i = 0; i < 200; ++i) {
+      damper.Update(0.1);
+      EXPECT_LT(old_current, damper.current()) << i;
+      old_current = damper.current();
+    }
+    EXPECT_NEAR(damper.current(), target, 1e-3 * target);
+  }
+}
+
+TEST(SmootherTest, SnapToTarget) {
+  double target = 20.0;
+  SmoothCriticalDamped<double> snap(/*smooth_time=*/0.0, /*start=*/0.0);
+  SmoothCriticalDamped<double> damper(/*smooth_time=*/2.0, /*start=*/0.0);
+  damper.set_target(target);
+  for (int i = 0; i < 200; ++i) {
+    damper.Update(0.1);
+    snap.set_target(damper.current());
+    EXPECT_NE(damper.current(), snap.current());
+    snap.Update(0.1);
+    EXPECT_EQ(damper.current(), snap.current());
+  }
+}
+
+TEST(SmootherTest, PreCalculated) {
+  SmoothCriticalDamped<double> damper(/*smooth_time=*/2.0 / 6.0, /*start=*/0.0);
+  damper.set_target(1.0);
+  damper.set_velocity(6.0);
+  double dt = 1.0 / 60.0;
+  damper.Update(dt);
+  EXPECT_NEAR(damper.current(), 0.09516258196, 1e-9);
+  damper.Update(dt);
+  EXPECT_NEAR(damper.current(), 0.1812692469, 1e-9);
+}
+
+TEST(SmootherTest, PreCalculatedExpDecayOneStep) {
+  SmoothExponentialDecayAt60<double> damper(
+      /*smooth_time=*/ConvertExpAt60FpsToSmoothTime(0.1), /*start=*/0.0);
+  damper.set_target(1.0);
+  damper.set_velocity(0.0);
+  double dt2 = 2.0 / 60.0;
+  damper.Update(dt2);
+  EXPECT_NEAR(damper.velocity(), 0.18 / dt2, 1e-9);
+  EXPECT_NEAR(damper.current(), 1.0 - 0.81, 1e-9);
+}
+
+TEST(SmootherTest, PreCalculatedExpDecayTwoStep) {
+  SmoothExponentialDecayAt60<double> damper(
+      /*smooth_time=*/ConvertExpAt60FpsToSmoothTime(0.1), /*start=*/0.0);
+  damper.set_target(1.0);
+  damper.set_velocity(0.0);
+  double dt = 1.0 / 60.0;
+  damper.Update(dt);
+  EXPECT_NEAR(damper.current(), 0.1, 1e-9);
+  damper.Update(dt);
+  EXPECT_NEAR(damper.velocity(), 0.18 / (dt * 2.0), 1e-9);
+  EXPECT_NEAR(damper.current(), 1.0 - 0.81, 1e-9);
+}
+
+TEST(SmootherTest, Vector3d) {
+  SmoothCriticalDamped<Eigen::Vector3d> damper(2.0 / 6.0, {0.0, 0.0, 0.0});
+  damper.set_target({1.0, 2.0, 3.0});
+  damper.set_velocity({6.0, 6.0, 6.0});
+  damper.Update(1.0 / 60.0);
+  EXPECT_NEAR(damper.current()[0], 0.09516258196, 1e-9);
+}
+
+TEST(SmootherTest, VectorExp3d) {
+  SmoothExponentialDecayAt60<Eigen::Vector3d> damper(
+      /*smooth_time=*/ConvertExpAt60FpsToSmoothTime(0.1),
+      /*start=*/{0.0, 0.0, 0.0});
+  damper.set_target({1.0, 2.0, 3.0});
+  damper.Update(1.0 / 60.0);
+  EXPECT_NEAR(damper.current()[0], 0.1, 1e-9);
+}
+
+// Angle class in degrees representing the range [-180, 180).
+class AngleDegrees {
+ public:
+  explicit AngleDegrees(double angle) : angle_(Normalize180(angle)) {}
+
+  // Shotest angular path favouring -180.0.
+  double operator-(AngleDegrees rhs) const {
+    return Normalize180(angle_ - rhs.angle_);
+  }
+
+  AngleDegrees operator-(double deltaAngle) const {
+    return AngleDegrees(angle_ - deltaAngle);
+  }
+
+  AngleDegrees operator+(double deltaAngle) const {
+    return AngleDegrees(angle_ + deltaAngle);
+  }
+
+  double angle() const { return angle_; }
+
+ private:
+  // Returns value in range [-180, 180)
+  static double Normalize180(double in) {
+    return in - 360 * std::floor((in + 180.0) / 360.0);
+  }
+
+  double angle_;
+};
+
+TEST(SmootherTest, AngleDegrees) {
+  SmoothCriticalDamped<AngleDegrees, double> damper(2.0 / 6.0,
+                                                    AngleDegrees(160.0));
+  damper.set_target(AngleDegrees(-160));
+  for (int i = 0; i < 100; ++i) {
+    damper.Update(1.0 / 60.0);
+    double val = damper.current().angle();
+    if (val > 0) {
+      EXPECT_GT(val, 160);
+    } else {
+      EXPECT_LT(val, -160);
+    }
+  }
+}
+
+}  // namespace
+}  // namespace util
+}  // namespace lab
+}  // namespace deepmind
