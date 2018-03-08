7efa590c9383e16d4f906f244a5de1d671eab7c4
cbeattie <cbeattie@google.com>
2018-03-08 19:06:55 +0000

    [skins] Switch default skin to 'crash'.

--- engine/code/cgame/cg_local.h
+++ engine/code/cgame/cg_local.h
@@ -80,13 +80,13 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define TEAM_OVERLAY_MAXNAME_WIDTH	12
 #define TEAM_OVERLAY_MAXLOCATION_WIDTH	16
 
-#define	DEFAULT_MODEL			"sarge"
+#define	DEFAULT_MODEL			"crash"
 #ifdef MISSIONPACK
 #define	DEFAULT_TEAM_MODEL		"james"
 #define	DEFAULT_TEAM_HEAD		"*james"
 #else
-#define	DEFAULT_TEAM_MODEL		"sarge"
-#define	DEFAULT_TEAM_HEAD		"sarge"
+#define	DEFAULT_TEAM_MODEL		"crash"
+#define	DEFAULT_TEAM_HEAD		"crash"
 #endif
 
 #define DEFAULT_REDTEAM_NAME		"Stroggs"
