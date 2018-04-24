41ca916d37bd4c9c86aa9f847f76ee212d21e070
cbeattie <cbeattie@google.com>
2018-04-24 11:22:02 +0100

    [example] Add Python environment interaction examples.

---
+++ examples/game_main.py
@@ -0,0 +1,92 @@
+## Copyright (C) 2016-17 Google Inc.
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License along
+## with this program; if not, write to the Free Software Foundation, Inc.,
+## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+################################################################################
+"""A working example of deepmind_lab using python."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import pprint
+import sys
+import numpy as np
+
+import deepmind_lab
+
+
+def run(level_script, config, num_episodes):
+  """Construct and start the environment."""
+  env = deepmind_lab.Lab(level_script, ['RGB_INTERLACED'], config)
+  env.reset()
+
+  observation_spec = env.observation_spec()
+  print('Observation spec:')
+  pprint.pprint(observation_spec)
+
+  action_spec = env.action_spec()
+  print('Action spec:')
+  pprint.pprint(action_spec)
+
+  obs = env.observations()  # dict of Numpy arrays
+  rgb_i = obs['RGB_INTERLACED']
+  print('Observation shape:', rgb_i.shape)
+  sys.stdout.flush()
+
+  # Create an action to move forwards.
+  action = np.zeros([7], dtype=np.intc)
+  action[3] = 1
+
+  score = 0
+  for _ in xrange(num_episodes):
+    while env.is_running():
+      # Advance the environment 4 frames while executing the action.
+      reward = env.step(action, num_steps=4)
+
+      if reward != 0:
+        score += reward
+        print('Score =', score)
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('-l', '--level_script', type=str,
+                      default='seekavoid_arena_01',
+                      help='The level that is to be played. Levels'
+                      'are Lua scripts, and a script called \"name\" means that'
+                      'a file \"assets/game_scripts/name.lua is loaded.')
+  parser.add_argument('-s', '--level_settings', type=str, default=None,
+                      action='append',
+                      help='Applies an opaque key-value setting. The setting is'
+                      'available to the level script. This flag may be provided'
+                      'multiple times. Universal settings are `width` and '
+                      '`height` which give the screen size in pixels, '
+                      '`fps` which gives the frames per second, and '
+                      '`random_seed` which can be specified to ensure the '
+                      'same content is generated on every run.')
+  parser.add_argument('--runfiles_path', type=str, default=None,
+                      help='Set the runfiles path to find DeepMind Lab data')
+  parser.add_argument('--num_episodes', type=int, default=1,
+                      help='The number of episodes to play.')
+  args = parser.parse_args()
+
+  # Convert list of level setting strings (of the form "key=value") into a
+  # `config` key/value dictionary.
+  config = {k:v for k, v in [s.split('=') for s in args.level_settings]}
+
+  if args.runfiles_path:
+    deepmind_lab.set_runfiles_path(args.runfiles_path)
+  run(args.level_script, config, args.num_episodes)
---
+++ python/random_agent_simple.py
@@ -0,0 +1,81 @@
+# Copyright 2016-17 Google Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+################################################################################
+"""A simple example of a random agent in deepmind_lab."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import random
+import numpy as np
+
+import deepmind_lab
+
+
+class RandomAgent(object):
+  """Basic random agent for DeepMind Lab."""
+  def __init__(self, action_spec):
+    self.action_spec = action_spec
+    self.action_count = len(action_spec)
+
+  def step(self):
+    """Choose a random amount of a randomly selected action."""
+    action_choice = random.randint(0, self.action_count - 1)
+    action_amount = random.randint(self.action_spec[action_choice]['min'],
+                                   self.action_spec[action_choice]['max'])
+    action = np.zeros([self.action_count], dtype=np.intc)
+    action[action_choice] = action_amount
+    return action
+
+
+def run(width, height, level_script, frame_count):
+  """Spins up an environment and runs the random agent."""
+  config = {'width': str(width), 'height': str(height)}
+  env = deepmind_lab.Lab(level_script, ['RGB_INTERLACED'], config=config)
+  env.reset()
+
+  reward = 0
+  agent = RandomAgent(env.action_spec())
+  for _ in xrange(frame_count):
+    if not env.is_running():
+      print('Environment stopped early')
+      env.reset()
+      agent.reset()
+    action = agent.step()
+    reward += env.step(action, num_steps=1)
+
+  print('Finished after %i steps. Total reward received is %f'
+        % (frame_count, reward))
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument('--frame_count', type=int, default=1000,
+                      help='Number of steps to run the agent')
+  parser.add_argument('--width', type=int, default=80,
+                      help='Horizontal size of the observations')
+  parser.add_argument('--height', type=int, default=80,
+                      help='Vertical size of the observations')
+  parser.add_argument('--runfiles_path', type=str, default=None,
+                      help='Set the runfiles path to find DeepMind Lab data')
+  parser.add_argument('--level_script', type=str, default='tests/demo_map',
+                      help='The environment level script to load')
+
+  args = parser.parse_args()
+  if args.runfiles_path:
+    deepmind_lab.set_runfiles_path(args.runfiles_path)
+  run(args.width, args.height, args.level_script, args.frame_count)
