google-deepmind/lab

commit 16f099ed6b223f16743e91009a5de1868c9f9289

[deepmind, public, testing] Use Abseil hash maps

deepmind/engine/BUILD
@@ -43,6 +43,7 @@ cc_library(
         "//deepmind/tensor:lua_tensor",
         "//deepmind/tensor:tensor_view",
         "//third_party/rl_api:env_c_api",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/strings",
     ],
 )
@@ -59,6 +60,7 @@ cc_library(
         "//deepmind/tensor:lua_tensor",
         "//deepmind/tensor:tensor_view",
         "//third_party/rl_api:env_c_api",
+        "@com_google_absl//absl/container:flat_hash_map",
     ],
 )
 
@@ -165,6 +167,7 @@ cc_library(
         "//deepmind/lua:read",
         "//deepmind/lua:table_ref",
         "//deepmind/support:logging",
+        "@com_google_absl//absl/container:flat_hash_map",
     ],
 )
 
@@ -292,6 +295,7 @@ cc_library(
         "//deepmind/lua:read",
         "//deepmind/lua:table_ref",
         "//deepmind/util:files",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/strings",
     ],
 )

deepmind/engine/context_events.h
@@ -20,9 +20,9 @@
 #define DML_DEEPMIND_ENGINE_CONTEXT_EVENTS_H_
 
 #include <string>
-#include <unordered_map>
 #include <vector>
 
+#include "absl/container/flat_hash_map.h"
 #include "deepmind/lua/lua.h"
 #include "deepmind/lua/n_results_or.h"
 #include "third_party/rl_api/env_c_api.h"
@@ -103,7 +103,7 @@ class ContextEvents {
   std::vector<const char*> names_;
 
   // Reverse lookup of event_name to type_id.
-  std::unordered_map<std::string, int> name_to_id_;
+  absl::flat_hash_map<std::string, int> name_to_id_;
 
   // Event observation storage.
   std::vector<std::vector<int>> shapes_;

BUILD
@@ -934,6 +934,7 @@ cc_library(
         ":file_reader_types",
         ":level_cache_types",
         "//third_party/rl_api:env_c_api",
+        "@com_google_absl//absl/container:flat_hash_map",
     ],
 )
 

deepmind/level_generation/text_level/BUILD
@@ -107,6 +107,7 @@ cc_library(
         "//deepmind/lua:read",
         "//deepmind/lua:table_ref",
         "//deepmind/support:logging",
+        "@com_google_absl//absl/container:flat_hash_map",
     ],
 )
 

deepmind/lua/BUILD
@@ -17,7 +17,10 @@ cc_library(
     name = "vm",
     srcs = ["vm.cc"],
     hdrs = ["vm.h"],
-    deps = [":lua"],
+    deps = [
+        ":lua",
+        "@com_google_absl//absl/container:flat_hash_map",
+    ],
 )
 
 cc_library(
@@ -119,6 +122,7 @@ cc_library(
     hdrs = ["push.h"],
     deps = [
         ":lua",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",
@@ -133,6 +137,7 @@ cc_test(
     deps = [
         ":push",
         ":vm_test_util",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",
@@ -156,6 +161,7 @@ cc_library(
     hdrs = ["read.h"],
     deps = [
         ":lua",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",
@@ -171,6 +177,7 @@ cc_test(
         ":push",
         ":read",
         ":vm_test_util",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/container:inlined_vector",
         "@com_google_absl//absl/strings",
         "@com_google_absl//absl/types:span",

deepmind/model_generation/BUILD
@@ -10,6 +10,7 @@ cc_library(
     deps = [
         ":transform",
         "//deepmind/support:logging",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_absl//absl/strings",
         "@eigen_archive//:eigen",
     ],
@@ -86,7 +87,10 @@ cc_library(
     name = "model",
     hdrs = ["model.h"],
     visibility = ["//visibility:public"],
-    deps = [":transform"],
+    deps = [
+        ":transform",
+        "@com_google_absl//absl/container:flat_hash_map",
+    ],
 )
 
 cc_library(

testing/BUILD
@@ -22,6 +22,7 @@ cc_test(
         "//:dmlablib",
         "//deepmind/support:test_srcdir",
         "//deepmind/util:files",
+        "@com_google_absl//absl/container:flat_hash_map",
         "@com_google_googletest//:gtest_main",
     ],
 )

deepmind/engine/context.cc
@@ -27,10 +27,10 @@
 #include <iterator>
 #include <limits>
 #include <string>
-#include <unordered_map>
 #include <utility>
 
 #include "deepmind/support/logging.h"
+#include "absl/container/flat_hash_map.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_join.h"
 #include "absl/strings/str_split.h"
@@ -950,7 +950,7 @@ bool Context::UpdatePlayerInfo(int player_id, char* info, int info_size) {
   script_table_ref_.PushMemberFunction("playerModel");
   if (!lua_isnil(L, -2)) {
     lua::Push(L, player_id + 1);
-    std::unordered_map<std::string, absl::string_view> player_info =
+    absl::flat_hash_map<std::string, absl::string_view> player_info =
         absl::StrSplit(info + 1, '\\');
     lua::Push(L, player_info["name"]);
     lua::Push(L, player_info["model"]);

commit 640fdf6d74fc50d569bd8dd5aeed9f47de0ed5b2

[context] Reinstate node stability by changing absl::flat_hash_map to absl::node_hash_map.
Stability was accidentally broken by commit 16f099ed6b223f16743e91009a5de1868c9f9289 when moving from std::unordered_map to Abseil maps.

deepmind/engine/BUILD
@@ -61,7 +61,7 @@ cc_library(
         "//deepmind/tensor:lua_tensor",
         "//deepmind/tensor:tensor_view",
         "//third_party/rl_api:env_c_api",
-        "@com_google_absl//absl/container:flat_hash_map",
+        "@com_google_absl//absl/container:node_hash_map",
     ],
 )
 

deepmind/engine/context_events.h
@@ -22,7 +22,7 @@
 #include <string>
 #include <vector>
 
-#include "absl/container/flat_hash_map.h"
+#include "absl/container/node_hash_map.h"
 #include "deepmind/lua/lua.h"
 #include "deepmind/lua/n_results_or.h"
 #include "third_party/rl_api/env_c_api.h"
@@ -99,11 +99,10 @@ class ContextEvents {
   // Events generated since construction or last call to Clear().
   std::vector<Event> events_;
 
-  // Lookup type_id to event_name.
+  // Bidirectional lookup for the mapping between type_id and event_name.
+  // Strings in the primary container (the map) need to be stable.
   std::vector<const char*> names_;
-
-  // Reverse lookup of event_name to type_id.
-  absl::flat_hash_map<std::string, int> name_to_id_;
+  absl::node_hash_map<std::string, int> name_to_id_;
 
   // Event observation storage.
   std::vector<std::vector<int>> shapes_;