ebc43be53c42f2e19e8805cfa83d378c6f3220c8
tkoeppe <tkoeppe@google.com>
2019-07-24 13:45:28 +0100

    [level_generation] Improve robustness of compile_map_test, simplifications

--- deepmind/level_generation/compile_map_test.cc
+++ deepmind/level_generation/compile_map_test.cc
@@ -7,6 +7,7 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/strings/str_cat.h"
 
 namespace deepmind {
 namespace lab {
@@ -21,21 +22,19 @@ struct LengthAndMd5 {
 // Manual checking can be done in bash with e.g.:
 // printf 'A%.0s' {1..2049} | md5sum
 constexpr LengthAndMd5 kMD5TestCases[] = {
-    {0, "d41d8cd98f00b204e9800998ecf8427e"},
+    {   0, "d41d8cd98f00b204e9800998ecf8427e"},
     {2047, "5972cf2ae2e853d61a6fa4cea47817cf"},
     {2048, "ef4ec7e7b54da951a91e8d10c85ca394"},
     {2049, "c3501f191a4a16b3f6bdef03c4beb26c"},
 };
 
 TEST(CompileMapTest, CorrectMD5Hashes) {
-  const std::string test_file_name =
-      std::string(std::getenv("TEST_TMPDIR")) + "/test_file";
+  const char* tmpdir = std::getenv("TEST_TMPDIR");
+  ASSERT_TRUE(tmpdir != nullptr) << "Missing TEST_TMPDIR environment variable";
   for (const LengthAndMd5& test_case : kMD5TestCases) {
-    std::ofstream file(test_file_name, std::ios::binary);
-    for (int i = 0; i < test_case.length; i++) {
-      file.write("A", 1);
-    }
-    file.close();
+    const int n = test_case.length;
+    const std::string test_file_name = absl::StrCat(tmpdir, "/test_file_", n);
+    std::ofstream(test_file_name, std::ios::binary) << std::string(n, 'A');
     EXPECT_EQ(test_case.md5, internal::CalculateMd5(test_file_name));
   }
 }
